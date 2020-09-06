09946e073b06699fbb6cc73b635a02a04fcf5f42
sikshya <sikshya.maharjan@joinsage.com>
2020-09-06 17:41:53 +0545

    chore: replace lorem picsum random with specific id

--- lib/core/services/post_service.dart
+++ lib/core/services/post_service.dart
@@ -39,11 +39,11 @@ class PostService {
               List<dynamic> imageUrls = [];
               if (index <= 8) {
                 for (int i = 0; i < index; i++) {
-                  imageUrls.add("https://picsum.photos/1069");
+                  imageUrls.add("https://picsum.photos/1068");
                 }
               } else {
                 for (int i = 0; i < index % 4; i++) {
-                  imageUrls.add("https://picsum.photos/1069");
+                  imageUrls.add("https://picsum.photos/1068");
                 }
               }
               return Post(message: quote.quote, imageList: imageUrls);
